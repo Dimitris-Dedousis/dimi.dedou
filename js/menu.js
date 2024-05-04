8bb234a1376dadf945bbc58da3717003aa061b37
Dimitris-Dedousis <72694765+Dimitris-Dedousis@users.noreply.github.com>
2024-05-04 16:22:40 +0300

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -2,19 +2,13 @@ $(document).ready(function() {
     
     var  menuLink = '<li><a href="./index">Αρχική Σελίδα</a></li>';
     $('.sidebarMenuInner').append(menuLink);
-    var  menuLink = '<li><a href="./experience">Αρχική Σελίδα</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
     var  menuLink = '<li><a href="./education">Εκπαίδευση</a></li>';
     $('.sidebarMenuInner').append(menuLink);
     var  menuLink = '<li><a href="./skills">Δεξιότητες</a></li>';
     $('.sidebarMenuInner').append(menuLink);
     var  menuLink = '<li><a href="./project">Project</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    
-   
+    $('.sidebarMenuInner').append(menuLink);  
     menuLink = '<li style="height: 200px;"></li>';
     $('.sidebarMenuInner').append(menuLink);
-    
-    
   });
   
