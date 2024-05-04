80e13ed308e393b6041a3484dddf56cfc8358575
Dimitris-Dedousis <72694765+Dimitris-Dedousis@users.noreply.github.com>
2024-05-04 21:43:07 +0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -7,7 +7,9 @@ $(document).ready(function() {
     var  menuLink = '<li><a href="./skills">Δεξιότητες</a></li>';
     $('.sidebarMenuInner').append(menuLink);
     var  menuLink = '<li><a href="./project">Project</a></li>';
-    $('.sidebarMenuInner').append(menuLink);  
+    $('.sidebarMenuInner').append(menuLink); 
+    var  menuLink = '<li><a href="./github">GitHub</a></li>';
+    $('.sidebarMenuInner').append(menuLink); 
     menuLink = '<li style="height: 200px;"></li>';
     $('.sidebarMenuInner').append(menuLink);
   });
