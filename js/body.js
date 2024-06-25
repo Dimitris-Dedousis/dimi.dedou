711b9d5c87c25f531a6b8c217a5c55b0a7bb5e28
Dimitris-Dedousis <72694765+dimidedou@users.noreply.github.com>
2024-06-25 11:20:04 +0300

    Update body.js

--- js/body.js
+++ js/body.js
@@ -11,12 +11,7 @@ const htmlContent = `
     <div id="sidebarMenu" style="overflow: auto;">
         <ul class="sidebarMenuInner"></ul>
     </div>
-    <ul class="social">
-        <li class="social-item"> <a class="facebook" href="https://www.facebook.com/dimidedou" target="_blank"><span></span> <span></span> <span></span> <span></span><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-        <li class="social-item"> <a class="instagram" href="https://www.instagram.com/dimi.dedou" target="_blank"><span></span> <span></span> <span></span> <span></span><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-        <li class="social-item"> <a class="github" href="https://github.com/dimidedou" target="_blank"><span></span> <span></span> <span></span> <span></span><i class="fa fa-github" aria-hidden="true"></i></a></li>
-        <li class="social-item"> <a class="linkedin" href="https://www.linkedin.com/in/dimidedou/" target="_blank"><span></span> <span></span> <span></span> <span></span><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-      </ul>
+
 `;
 
 // Εύρεση του στόχου με το id 'add_headder'
